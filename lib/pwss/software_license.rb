0dc27a41a003a93a02b33772c4f48b31b7ce9915
Adolfo Villafiorita <adolfo.villafiorita@me.com>
2014-09-21 19:19:31 +0200

    support for software licenses

---
+++ lib/pwss/software_license.rb
@@ -0,0 +1,20 @@
+require 'pwss/entry'
+
+module Pwss
+  class SoftwareLicense < Entry
+    def initialize
+      super
+      @fields = {
+        "title"          => ["Readline.readline('title: ')", "'title'"],
+        "version"        => ["Readline.readline('version: ')", "0.0.1"],
+        "license_number" => ["Readline.readline('license number: ')", "000-0000-0000-0000"],
+        "licensed_to"    => ["Readline.readline('licensed to: ')", "'John Doe'"],
+        "email"          => ["Readline.readline('email: ')", "jdoe@example.com"],
+        "created_at"     => ["", "Date.today"],
+        "updated_at"     => ["", "nil"],
+        "url"            => ["Readline.readline('url: ')", "''"],
+        "notes"          => ["get_lines",   "''"]
+      }
+    end
+  end
+end
